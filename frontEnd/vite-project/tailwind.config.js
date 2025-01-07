9172814025a3def5a5023e0ebd258910f1037c51
Sayali098 <shelakesayali9022@gmail.com>
2025-01-07 21:40:37 +0530

    change tailwind

--- frontEnd/vite-project/tailwind.config.js
+++ frontEnd/vite-project/tailwind.config.js
@@ -1,16 +1,15 @@
-// /** @type {import('tailwindcss').Config} */
+import lineClamp from '@tailwindcss/line-clamp';
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-
   theme: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/line-clamp'),
-    
+    lineClamp,
   ],
 }
 
