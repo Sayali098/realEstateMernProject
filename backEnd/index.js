39c806e9ca9d5bf2658b6b05f1f83d96018e6c8a
Sayali098 <shelakesayali9022@gmail.com>
2025-01-07 21:21:00 +0530

    change index.js

--- backEnd/index.js
+++ backEnd/index.js
@@ -39,11 +39,11 @@ mongoose
   app.use('/api/listing',listingRouter)
   
 
-  app.use(express.static(path.join(__dirname,'/frontEnd/dist')))
+  app.use(express.static(path.join(__dirname,'/frontEnd/vite-project/dist')))
   
   app.get('*',(req,res)=>{
 
-    res.sendFile(path.join(__dirname,'frontEnd','dist','index.html'))
+    res.sendFile(path.join(__dirname,'frontEnd/vite-project','dist','index.html'))
   })
 
   app.use((err,req,res,next)=>{
